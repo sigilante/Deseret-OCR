65122ecbcf1f49ba28491c3d108d57377ada86ea
rshosted <79804224+rshosted@users.noreply.github.com>
2022-01-28 21:02:49 -0600

    pad characters

--- charproc.m
+++ charproc.m
@@ -1,13 +1,16 @@
-function [padchar] = charproc(mydir)
+function [padchar,maxhidx,maxwidx] = charproc(mydir)
+% Pad all characters to a uniform height and width corresponding to the
+% tallest (left-bracket-glyph) and widest (g-glyph) characters
 
 myfiles = dir(fullfile(mydir,'*.png'));
 for i = 1:length(myfiles)
     mychar{i,1} = imread(strcat(mydir,myfiles(i,1).name));
     mysize(i,:) = size(mychar{i,1});
 end
-maxh = max(mysize(:,1));
-maxw = max(mysize(:,2));
+[maxh,maxhidx] = max(mysize(:,1));
+[maxw,maxwidx] = max(mysize(:,2));
 
 for i = 1:length(myfiles)
-    padchar{i,1} = padarray(mychar{i,1},[myh,myw]);
+    mydim(i,:) = size(mychar{i,1});
+    padchar{i,1} = padarray(mychar{i,1},[maxh-mydim(i,1),maxw-mydim(i,2)]);
 end
